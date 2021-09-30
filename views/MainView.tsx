3349773cf45db52a2176645bea7317fabe846694
David Carpenter <carpenterd777@gmail.com>
2021-09-29 23:57:15 -0400

    add stroll button

--- views/MainView.tsx
+++ views/MainView.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Dimensions, View } from "react-native";
 import MapView from "react-native-maps";
+import StrollButton from "../components/StrollButton";
 
 const MainView = (): JSX.Element => {
 	return (
 		<View style={styles.container}>
 			<MapView style={styles.map} />
+			<StrollButton/>
 		</View>
 	);
 };
