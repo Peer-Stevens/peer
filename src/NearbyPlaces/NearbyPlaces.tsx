e267dc7b63bd4872cc012a506fb82bc516b97b2d
Andrew Jones <aej11@hotmail.com>
2021-10-01 17:30:35 -0400

    Added component to fetch and displace nearby places

--- src/NearbyPlaces/NearbyPlaces.tsx
+++ src/NearbyPlaces/NearbyPlaces.tsx
@@ -3,17 +3,18 @@ import { Text } from "react-native";
 import { useLocation } from "./useLocation";
 import Location from "expo-location";
 import axios from "axios";
+import { SERVER_BASE_URL } from "@env";
+import type { Place } from "@googlemaps/google-maps-services-js";
 
 export const NearbyPlaces = () => {
-	const { location } = useLocation()
-	const [nearbyPlaces, setNearbyPlaces] = React.useState<any>();
+	const { location } = useLocation();
+	const [nearbyPlaces, setNearbyPlaces] = React.useState<Place[]>();
 
 	const getNearbyPlaces = async (location: Location.LocationObject) => {
-		const result = await axios.get(
-			process.env.SERVER_BASE_URL! +
-				`/getNearbyPlaces?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}`
+		const result = await axios.get<{ places: Place[] }>(
+			`${SERVER_BASE_URL}/getNearbyPlaces?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}`
 		);
-		setNearbyPlaces(result);
+		setNearbyPlaces(result.data.places);
 	};
 
 	useEffect(() => {
@@ -23,5 +24,15 @@ export const NearbyPlaces = () => {
 		}
 	}, [location]);
 
-	return <Text>{JSON.stringify(nearbyPlaces)}</Text>;
+	if (nearbyPlaces && nearbyPlaces.length > 0) {
+		return (
+			<Text>
+				You are walking by{" "}
+				{nearbyPlaces
+					.map(place => place.name)
+					.slice(0, 3)
+					.join(", ")}
+			</Text>
+		);
+	} else return null;
 };
