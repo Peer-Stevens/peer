16f0e8f3b28b902f58a2a83f0c465e00339d80f2
David Carpenter <carpenterd777@gmail.com>
2021-10-02 14:42:08 -0400

    ran the linter

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { cleanup } from "@testing-library/react-native"
+import { cleanup } from "@testing-library/react-native";
 import TestRenderer from "react-test-renderer";
 import App from "../App";
 
 // there will be errors if the tree is not unmounted after each test
 // https://callstack.github.io/react-native-testing-library/docs/api#cleanup
-afterEach(cleanup)
+afterEach(cleanup);
 
 const tr = TestRenderer.create(<App />);
 
