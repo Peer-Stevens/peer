631844b62b67ff365142af22754172e13195fc19
David Carpenter <carpenterd777@gmail.com>
2021-10-03 14:21:30 -0400

    add filter box component

--- components/FilterBox.tsx
+++ components/FilterBox.tsx
@@ -1,16 +1,35 @@
-import React from "react"
-import BouncyCheckbox from "react-native-bouncy-checkbox"
-import * as Peer from "./Peer/lib"
+import React from "react";
+import * as Peer from "./Peer/lib";
 
-const FilterBox = () : JSX.Element => {
-    return (
-        <Peer.Box accessibilityLabel="List of filters">
-            <BouncyCheckbox
-                onPress={(isChecked) => {}}
-                text="Grocery stores"
-            />
-        </Peer.Box>
-    )
+export interface FilterBoxProps {
+	//eslint-disable-next-line @typescript-eslint/ban-types
+	style?: object; // may be unsafe, but this is the type provided
+	// by Stylesheet documentation:
+	// https://reactnative.dev/docs/stylesheet#compose
 }
 
-export default FilterBox
+const FilterBox = ({style}: FilterBoxProps): JSX.Element => {
+	const categories = [
+		//FIXME: use actual categories and not placeholders
+		"Grocery stores",
+		"Parks",
+		"Museum",
+	];
+	return (
+		<Peer.Box accessibilityLabel="List of filters" style={style}>
+			{categories.map((category: string, index: number) => {
+				return (
+					<Peer.CheckBox
+						text={category}
+						key={index}
+						accessibilityLabel={`Filter out type ${category}`}
+						value={false}
+						onValueChange={() => {}} //FIXME: this does nothing
+					/>
+				);
+			})}
+		</Peer.Box>
+	);
+};
+
+export default FilterBox;
