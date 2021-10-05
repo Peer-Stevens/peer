31f15f60f2ca846ee0f31ddb643fd62f51ee2543
Andrew Jones <aej11@hotmail.com>
2021-10-05 16:19:36 -0400

    Reverted change to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
 	overrides: [
 		{
 			files: ["*.ts", "*.tsx"],
-			//extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
+			extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
 			parserOptions: {
 				project: ["./tsconfig.json"],
 			},
