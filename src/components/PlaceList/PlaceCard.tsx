dc8c36e135c2d69a979e6e68a3b2be487a40dca7
jlora23 <48076390+jlora23@users.noreply.github.com>
2021-10-11 21:56:39 -0400

    Override eslint

--- src/components/PlaceList/PlaceCard.tsx
+++ src/components/PlaceList/PlaceCard.tsx
@@ -33,6 +33,7 @@ const PlaceCard: React.FC<PlaceCardProps> = ({
 			>
 				<Image
 					style={styles.imageStyle}
+					/*eslint-disable-next-line @typescript-eslint/no-unsafe-assignment*/
 					source={require("../../../assets/restaurant.jpg")}
 				/>
 			</View>
