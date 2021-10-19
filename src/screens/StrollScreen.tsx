9888bded1bdf7c3887fdbe3e2ed824f9c6b3a4e3
David Carpenter <carpenterd777@gmail.com>
2021-10-18 20:54:27 -0400

    split up map screen from stroll screen, rename to screens

---
+++ src/screens/StrollScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import {View} from "react-native"
+import { NearbyPlaces } from "../components/NearbyPlaces/NearbyPlaces";
+
+export interface StrollScreenProps {
+    toggleIsStrolling: () => void;
+}
+
+const StrollScreen : React.FC<StrollScreenProps> = ({toggleIsStrolling}: StrollScreenProps) => {
+    return (
+        <View style={
+            {
+            flex: 1,
+            flexDirection: "column",
+            backgroundColor: "#fff",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
+            <NearbyPlaces stopStrolling={toggleIsStrolling} />
+        </View>
+    );
+}
+
+export default StrollScreen;
