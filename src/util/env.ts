1e52da4f756d133feda07fd08f216a609e85efd9
David Carpenter <carpenterd777@gmail.com>
2021-10-30 20:06:32 -0400

    Ran the linter (shame on me but in my defense I made the last commit by pressing . on github)

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,9 +1,9 @@
 /**
  * env.ts
- * 
- * This file is used in lieu of a `.env` file. It stores project-relevant global variables 
+ *
+ * This file is used in lieu of a `.env` file. It stores project-relevant global variables
  * here instead of in the system environment.
- * 
+ *
  * Anything added here should be data useful to several components at once throughout the
  * project, and must strictly be declared with `const`.
  */
