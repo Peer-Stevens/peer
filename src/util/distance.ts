06d3f52857046c53448aa3a5c8146db810dfc294
David Carpenter <carpenterd777@gmail.com>
2021-11-07 02:51:01 -0500

    Add disclaimer to docstrings

--- src/util/distance.ts
+++ src/util/distance.ts
@@ -3,9 +3,13 @@ export type CoordinatePair = {
 	longitude?: number;
 };
 
+const feetPerDegree = 362751.84;
+const feetPerMi = 5279.9869;
+
 /**
  * Computes the distance in feet between two coordinate pairs.
  * Returns undefined if any argument is undefined.
+ * Inaccurate for distances beyond the scale of 1 mile.
  * @param pair1 the first coordinate pair
  * @param pair2 the second coordinate pair
  * @returns distance in feet
@@ -21,7 +25,7 @@ export const computeDistanceFeet = (
 	const opposite = pair2.longitude - pair1.longitude;
 
 	const degreeDistance = Math.sqrt(adjacent ** 2 + opposite ** 2);
-	const distanceInFeet = Math.round(degreeDistance * 362751.84);
+	const distanceInFeet = Math.round(degreeDistance * feetPerDegree);
 
 	return distanceInFeet;
 };
@@ -29,6 +33,7 @@ export const computeDistanceFeet = (
 /**
  * Computes the distance in miles between two coordinate pairs.
  * Returns undefined if any argument is undefined.
+ * Inaccurate for use beyond the scale of 1000 feet.
  * @param pair1 the first coordinate pair
  * @param pair2 the second coordinate pair
  * @returns distance in miles
@@ -38,5 +43,5 @@ export const computeDistanceMi = (
 	pair2: CoordinatePair
 ): number | undefined => {
 	const distanceInFeet = computeDistanceFeet(pair1, pair2);
-	return distanceInFeet ? distanceInFeet / 5279.9869 : undefined;
+	return distanceInFeet ? distanceInFeet / feetPerMi : undefined;
 };
