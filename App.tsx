14dc7e3db038aab04f0a6cd58425f6584dcb96bc
Andrew Jones <aej11@hotmail.com>
2021-12-05 08:23:38 -0500

    began cleanup of nav

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
-// import React from "react";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React from "react";
 import MainScreen from "./src/screens/MainScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import CreateAccount from "./src/screens/CreateAccountScreen";
 
-const AppNavigator = createStackNavigator(
-	{
-		Home: MainScreen,
-		Login: LoginScreen,
-		CreateAccount: CreateAccount,
-	},
-	{
-		initialRouteName: "Login",
-	}
-);
-
-export default createAppContainer(AppNavigator);
+export default function App(): JSX.Element {
+	return <MainScreen />;
+}
