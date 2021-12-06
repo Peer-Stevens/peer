5f7d817744eba5afaafad7ffc9c6c989746e3c99
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-12-05 23:04:54 -0500

    almost done with create account page

--- src/screens/CreateAccountScreen.tsx
+++ src/screens/CreateAccountScreen.tsx
@@ -3,101 +3,129 @@ import { View, Text, TextInput, StyleSheet } from "react-native";
 import { Button } from "../components/Button";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useForm, Controller } from "react-hook-form";
 import { Screens } from "./MainScreen";
+import sha256 from "crypto-js/sha256";
 
 type CreateAccountScreenProps = { setPage: (screen: Screens) => void };
 
-type FormData = {
-	email: string;
-	password: string;
-};
-
 const CreateAccount: React.FC<CreateAccountScreenProps> = ({ setPage }) => {
 	const [errorMsg, setErrorMsg] = useState("");
-	const {
-		register,
-		// control,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormData>({
-		defaultValues: {
-			email: "",
-			password: "",
-		},
-	});
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-	const createUser = async (email: string, password: string) => {
+	/**
+	 * Validates that the email the user provided has a valid email format
+	 * @param email
+	 * @returns
+	 */
+	const validateEmail = (email: string) => {
 		setErrorMsg("");
 
-		try {
-			const { data } = await axios.post("https://peer-server-stevens.herokuapp.com/addUser", {
-				email: email,
-				hash: password, // i think i need to hash this
-				isBlindMode: false,
-				readsBraille: true,
-				doesNotPreferHelp: false,
-			});
-			// should be logging the user in, storing the token, and storing the hashed password here
-			setPage(Screens.Home);
-		} catch (e) {
-			// TODO could not figure out how to pass e.error to setErrorMsg without getting yelled out (couldn't figure out how to make the types behave)
-			console.log(e);
-			setErrorMsg("Something went wrong");
+		if (!email) {
+			setEmail("");
+			return;
 		}
+
+		const reg: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+		if (reg.test(email) === false) {
+			setErrorMsg("Please provide a valid email");
+			return;
+		} else {
+			setErrorMsg("");
+			setEmail(email);
+			return;
+		}
+	};
+
+	/**
+	 * Hashes the password the user provies
+	 * @param passowrd
+	 * @returns
+	 */
+	const hashPassword = (password: string) => {
+		setErrorMsg("");
+
+		if (!password) {
+			setPassword("");
+			return;
+		}
+		const hash: CryptoJS.lib.WordArray = sha256(password);
+
+		if (hash !== undefined) {
+			setPassword(hash.toString());
+			return;
+		} else {
+			setPassword("");
+			setErrorMsg("Something went wrong, please try again");
+			return;
+		}
+	};
+
+	const createUser = async () => {
+		setErrorMsg("");
+
+		if (!email || !password) {
+			setErrorMsg("Please enter a username and password to create account");
+			return;
+		}
+		// try {
+		// 	const { data } = await axios.post("https://peer-server-stevens.herokuapp.com/addUser", {
+		// 		email: email,
+		// 		hash: password,
+		// 		// not including these right now, so I'm defaulting them to these values
+		// 		isBlindMode: false,
+		// 		readsBraille: true,
+		// 		doesNotPreferHelp: false,
+		// 	});
+		// 	// should be logging the user in, storing the token, and storing the hashed password here
+		// 	setPage(Screens.Home);
+		// } catch (e) {
+		// 	// TODO could not figure out how to pass e.error to setErrorMsg without getting yelled out (couldn't figure out how to make the types behave)
+		// 	console.log(e);
+		// 	setErrorMsg("Something went wrong");
+		// }
 	};
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.label}>Email</Text>
-			{/* <Controller
-				control={control}
-				rules={{ required: true }}
-				render={({ field: { onChange, onBlur, value } }) => (
-					<TextInput
-						style={styles.input}
-						onBlur={onBlur}
-						onChangeText={value => onChange(value)}
-						value={value}
-					/>
-				)}
-				name="email"
-			/> */}
-			<TextInput {...register("email")} style={styles.input} />
-			{errors.email && <Text style={{ color: "white" }}>This is a required field</Text>}
-
-			<Text style={styles.label}>Password</Text>
-			{/* <Controller
-				control={control}
-				rules={{ required: true, maxLength: 100 }}
-				render={({ field: { onChange, onBlur, value } }) => (
-					<TextInput
-						style={styles.input}
-						onBlur={onBlur}
-						onChangeText={value => onChange(value)}
-						value={value}
-					/>
-				)}
-				name="password"
-			/>
-			{errors.email && <Text>This is a required field</Text>} */}
-
-			{/* <Button text="Log in" onPress={logInUser} accessibilityLabel="Click to log in" />
-			<Button
-				text="Create Account"
-				onPress={() => navigation.navigate("CreateAccount")}
-				accessibilityLabel="Click to create account"
-			/> */}
-			<TextInput {...register("password")} style={styles.input} />
-			{errors.password && <Text style={{ color: "white" }}>This is a required field</Text>}
-			{/* <Text style={{ fontSize: 30 }}>{errorMsg ? <Text>{errorMsg}</Text> : null}</Text> */}
-
-			<Button
-				text="Create Account"
-				onPress={() => {
-					handleSubmit(data => createUser(data.email, data.password));
+			<Text style={styles.text}>Create Account</Text>
+			<Text
+				style={{
+					color: "white",
+					fontWeight: "bold",
+					margin: 20,
+					marginLeft: 0,
+					fontSize: 30,
+					textAlign: "center",
 				}}
+			>
+				{errorMsg ? <Text>{errorMsg}</Text> : null}
+			</Text>
+			<Text style={styles.label}>Email</Text>
+			<TextInput
+				style={styles.input}
+				textContentType="emailAddress"
+				onChangeText={input => validateEmail(input)}
+			/>
+			<Text style={styles.label}>Password</Text>
+			<TextInput
+				style={styles.input}
+				textContentType="password"
+				onChangeText={input => hashPassword(input)}
+			/>
+			<Button
+				style={styles.button}
+				text="Create Account"
+				onPress={() => createUser()}
 				accessibilityLabel="Click to create account"
+			/>
+
+			<Button
+				style={styles.button}
+				text="Back to previous page"
+				onPress={() => setPage(Screens.NotLoggedIn)}
+				accessibilityLabel="Click to go back to previous page"
 			/>
 		</View>
 	);
@@ -111,11 +139,7 @@ const styles = StyleSheet.create({
 		fontSize: 35,
 	},
 	button: {
-		marginTop: 40,
-		color: "white",
-		height: 40,
-		backgroundColor: "#ec5990",
-		borderRadius: 4,
+		marginTop: 30,
 	},
 	container: {
 		flex: 1,
@@ -130,6 +154,14 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderRadius: 4,
 		fontSize: 35,
+	},
+	text: {
+		color: "white",
+		margin: 20,
+		marginLeft: 0,
+		fontSize: 40,
+		textAlign: "center",
+		paddingBottom: 10,
 	},
 });
 
