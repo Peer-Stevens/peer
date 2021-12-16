51244f265ee137a8aa54555a234e69625f0bb820
David Carpenter <carpenterd777@gmail.com>
2021-12-15 22:57:14 -0500

    Change config settings a bit

--- app.config.js
+++ app.config.js
@@ -1,16 +1,21 @@
 export default {
-	name: "peer",
+	name: "Peer",
+	description:
+		"An explorative navigation app for the blind and visually impaired for iOS and Android.",
 	owner: "peer-stevens",
 	slug: "peer",
 	sdkVersion: "43.0.0",
 	version: "1.2.0",
 	orientation: "portrait",
+	githubUrl: "https://github.com/Peer-Stevens/peer",
 	icon: "./assets/icon.png",
+	permissions: ["ACCESS_FINE_LOCATION", "ACCESS_BACKGROUND_LOCATION"],
 	splash: {
 		image: "./assets/splash.png",
 		resizeMode: "contain",
 		backgroundColor: "#ffffff",
 	},
+	primaryColor: "#ffffff",
 	updates: {
 		fallbackToCacheTimeout: 0,
 	},
@@ -22,6 +27,8 @@ export default {
 	},
 	android: {
 		adaptiveIcon: {
+			foregroundImage: "./assets/android-icon-foreground.png",
+			backgroundImage: "./assets/android-icon-background.png",
 			backgroundColor: "#FFFFFF",
 		},
 		package: "com.peer.peerstevens",
@@ -31,6 +38,13 @@ export default {
 				apiKey: process.env.MAPS_API_KEY,
 			},
 		},
+	},
+	androidStatusBar: {
+		barStyle: "dark-content",
+	},
+	androidNavigationBar: {
+		visible: "sticky-immersive",
+		barStyle: "light-content",
 	},
 	web: {
 		favicon: "./assets/favicon.png",
