060be2b8dc7e24619fd6514ae8e1912639066a9d
David Carpenter <carpenterd777@gmail.com>
2021-12-18 15:08:42 -0500

    add rating breakdown

--- src/screens/DetailedViewScreen.tsx
+++ src/screens/DetailedViewScreen.tsx
@@ -1,20 +1,27 @@
 //This is where we will be displaying the information of each single place
 import React from "react";
-import { StyleSheet, Dimensions, View, Text, ActivityIndicator, TextProps } from "react-native";
+import { StyleSheet, View, Text, ActivityIndicator, TextProps, ScrollView } from "react-native";
 import { computeDistanceMi } from "../util/distance";
 import { useLocation } from "../hooks/useLocation";
 import { Button } from "../components/Button";
-import { TEXT_COLOR } from "../util/colors";
 import { PlaceImage } from "../components/PlaceImage";
 import { useFetchPlace } from "../hooks/useFetchPlace";
-import { getAverageA11yRating } from "../util/processA11yRatings";
 
 export interface PlaceProps {
 	placeID: string;
 	setPage: (newPage: string) => void;
 }
 
-const BodyText = (props: TextProps) => <Text style={styles.text} {...props} />;
+const BodyText = (props: TextProps) => (
+	<Text
+		style={{
+			fontSize: 30,
+			lineHeight: 35,
+			marginBottom: 15,
+		}}
+		{...props}
+	/>
+);
 
 const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlaceProps) => {
 	const { placeDetails } = useFetchPlace({ placeID });
@@ -26,7 +33,7 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 		longitude: location?.coords.longitude,
 	};
 
-	if (placeDetails) {
+	if (placeDetails && placeDetails.placeDetails.accessibilityData) {
 		const place = placeDetails.placeDetails.result;
 
 		const placeCoord = {
@@ -36,8 +43,14 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 		const placeCoords = { latitude: placeCoord.latitude, longitude: placeCoord.longitude };
 		const distanceInMi = computeDistanceMi(userCoords, placeCoords)?.toPrecision(2);
 
+		// TODO: update to use new rating fields
+		const { avgBraille, avgFontReadability, avgGuideDogFriendly, avgNavigability } =
+			placeDetails.placeDetails.accessibilityData;
+
+		const a11yDataList = [avgBraille, avgFontReadability, avgGuideDogFriendly, avgNavigability];
+
 		return (
-			<View style={styles.border}>
+			<View style={{ flex: 1 }}>
 				<PlaceImage
 					photoref={place.photos?.[0]?.photo_reference}
 					placeName={place.name}
@@ -46,25 +59,66 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 						height: "40%",
 					}}
 				/>
-				<View style={styles.content}>
-					<Text ellipsizeMode="tail" numberOfLines={2} style={styles.title}>
-						{place.name}
-					</Text>
-					<BodyText ellipsizeMode="tail" numberOfLines={2}>
-						{place.formatted_address}
-					</BodyText>
-					<BodyText accessibilityLabel={distanceInMi ? `${distanceInMi} miles away` : ""}>
-						{distanceInMi ? `${distanceInMi} mi away` : ""}
-					</BodyText>
-					<BodyText>
-						{Math.round(
-							getAverageA11yRating({
-								accessibilityData: placeDetails.placeDetails.accessibilityData,
-							}) * 2
-						) / 2}
-					</BodyText>
+				<View
+					style={{
+						width: "90%",
+						alignSelf: "center",
+					}}
+				>
+					<ScrollView style={{ height: "40%" }}>
+						<Text
+							ellipsizeMode="tail"
+							numberOfLines={2}
+							style={{
+								fontSize: 35,
+								fontWeight: "bold",
+							}}
+						>
+							{place.name}
+						</Text>
+						<BodyText ellipsizeMode="tail" numberOfLines={2}>
+							{place.formatted_address}
+						</BodyText>
+						<BodyText
+							accessibilityLabel={distanceInMi ? `${distanceInMi} miles away` : ""}
+						>
+							{distanceInMi ? `${distanceInMi} mi away` : ""}
+						</BodyText>
+						{[
+							"Navigability",
+							"Sensory Aids",
+							"Staff Helpfulness",
+							"Guide Dog Friendliness",
+						].map((value, index) => {
+							return (
+								<View key={`rating${index}`}>
+									<Text
+										style={{
+											fontSize: 30,
+											lineHeight: 35,
+											marginBottom: 15,
+											alignSelf: "center",
+										}}
+									>
+										{value}
+									</Text>
+									<Text
+										style={{
+											fontSize: 30,
+											lineHeight: 35,
+											marginBottom: 15,
+											alignSelf: "center",
+											fontWeight: "bold",
+										}}
+									>
+										{a11yDataList[index]}
+									</Text>
+								</View>
+							);
+						})}
+					</ScrollView>
 					<Button
-						style={styles.submitButton}
+						style={styles.button}
 						onPress={() => {
 							//placeholder
 						}}
@@ -73,7 +127,7 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 						text="Submit a Rating"
 					/>
 					<Button
-						style={styles.homeBtn}
+						style={styles.button}
 						onPress={() => setPage("mapScreen")} //need to update this after Eleni's PR is merged w/ Andrew's updates to nav
 						accessibilityLabel="Return to Home Page"
 						text="Go Home"
@@ -85,7 +139,7 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 		return (
 			<View
 				style={{
-					height: Dimensions.get("window").height,
+					height: "100%",
 					display: "flex",
 					justifyContent: "center",
 				}}
@@ -97,34 +151,8 @@ const DetailedViewScreen: React.FC<PlaceProps> = ({ setPage, placeID }: PlacePro
 };
 
 const styles = StyleSheet.create({
-	border: {
-		width: Dimensions.get("window").width,
-		height: Dimensions.get("window").height,
-		borderColor: "black",
-		borderWidth: 1,
-	},
-	content: {
-		width: Dimensions.get("window").width * 0.95,
-		marginLeft: Dimensions.get("window").width * 0.025,
-	},
-	title: {
-		fontSize: 35,
-		fontWeight: "bold",
-	},
-	text: {
-		fontSize: 30,
-		lineHeight: 35,
-		marginBottom: 15,
-	},
-	homeBtn: {
-		borderWidth: 3,
-		borderColor: TEXT_COLOR,
-		width: Dimensions.get("window").width * 0.9,
-	},
-	submitButton: {
-		borderWidth: 3,
-		borderColor: TEXT_COLOR,
-		width: Dimensions.get("window").width * 0.9,
+	button: {
+		width: "100%",
 		marginBottom: 10,
 	},
 });
