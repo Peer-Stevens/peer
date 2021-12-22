06e1173b9c32997363b0eabc04e68d7ca0c6ca64
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-12-22 17:20:22 -0500

    decided to use a validator library instead to ensure things are secure

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import sha256 from "crypto-js/sha256";
+import { validate } from "email-validator";
 
 export const useAuthentication = (): {
 	validateEmail: (input: string) => void;
@@ -24,10 +25,7 @@ export const useAuthentication = (): {
 			return;
 		}
 
-		// source: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
-		const regEx =
-			/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-		if (regEx.test(input) === false) {
+		if (!validate(input.toLowerCase())) {
 			setErrorMsg("Please provide a valid email");
 		} else {
 			setErrorMsg("");
