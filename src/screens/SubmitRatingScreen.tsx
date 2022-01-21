c43915a3529af10d897d6cefcad64390a774a159
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-01-21 17:26:51 -0500

    deleted hook and created individual states

--- src/screens/SubmitRatingScreen.tsx
+++ src/screens/SubmitRatingScreen.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, View, Text } from "react-native";
 import { PlaceImage } from "../components/PlaceImage";
 import { Button } from "../components/Button";
 import { CANCEL, getIncrementRatingButtonLabel, PLACE_ATTRIBUTES, SUBMIT } from "../util/strings";
-import { useCounter } from "../hooks/useCounter";
 
 export interface SubmitRatingScreenProps {
 	placeID?: string;
@@ -12,11 +11,27 @@ export interface SubmitRatingScreenProps {
 }
 
 export const DEFAULT_INTERIM_RATING = 3;
+const MAX_COUNT = 5;
+const MIN_COUNT = 0;
+const INCREMENT_VAL = 0.5;
 
 const SubmitRatingScreen: React.FC<SubmitRatingScreenProps> = ({
 	placeName,
 	photo_reference,
 }: SubmitRatingScreenProps) => {
+	const [counter0, setCounter0] = useState<number>(DEFAULT_INTERIM_RATING);
+	const [counter1, setCounter1] = useState<number>(DEFAULT_INTERIM_RATING);
+	const [counter2, setCounter2] = useState<number>(DEFAULT_INTERIM_RATING);
+	const [counter3, setCounter3] = useState<number>(DEFAULT_INTERIM_RATING);
+
+	// 0 index is navigability, 1 is sensory aid, 2 is staff helpfulness, 3 is guide dog
+	const counterStates = [
+		{ state: counter0, update: setCounter0 },
+		{ state: counter1, update: setCounter1 },
+		{ state: counter2, update: setCounter2 },
+		{ state: counter3, update: setCounter3 },
+	];
+
 	return (
 		<ScrollView>
 			<PlaceImage photoref={photo_reference} placeName={placeName} />
@@ -29,7 +44,7 @@ const SubmitRatingScreen: React.FC<SubmitRatingScreenProps> = ({
 				}}
 			/>
 			{PLACE_ATTRIBUTES.map((attribute, index) => {
-				const { counter, increment, decrement } = useCounter(DEFAULT_INTERIM_RATING);
+				const counter = counterStates[index];
 
 				return (
 					<View key={`rating${index}`}>
@@ -37,23 +52,33 @@ const SubmitRatingScreen: React.FC<SubmitRatingScreenProps> = ({
 							iconName={"minus"}
 							accessibilityLabel={getIncrementRatingButtonLabel(
 								true,
-								counter,
+								counter.state,
 								attribute,
 								placeName
 							)}
-							onPress={() => decrement(counter)}
+							onPress={() => {
+								if (counter.state === MIN_COUNT) {
+									return;
+								}
+								counter.update(counter.state - INCREMENT_VAL);
+							}}
 						/>
 						<Text>{attribute}</Text>
-						<Text>{counter}</Text>
+						<Text>{counter.state}</Text>
 						<Button
 							iconName={"plus"}
 							accessibilityLabel={getIncrementRatingButtonLabel(
 								false,
-								counter,
+								counter.state,
 								attribute,
 								placeName
 							)}
-							onPress={() => increment(counter)}
+							onPress={() => {
+								if (counter.state === MAX_COUNT) {
+									return;
+								}
+								counter.update(counter.state + INCREMENT_VAL);
+							}}
 						/>
 					</View>
 				);
