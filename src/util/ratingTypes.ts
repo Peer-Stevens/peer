517044ef72456f2db751ee5bf8ba435f0ec64bcd
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-03-07 10:16:04 -0500

    removed old comment

--- src/util/ratingTypes.ts
+++ src/util/ratingTypes.ts
@@ -1,7 +1,5 @@
 import { Rating as PeerRatingType } from "peer-types";
 
-// TODO: export database types to own package
-// so that they do not need to be managed twice
 export interface Rating extends PeerRatingType {
 	// index signature: below defines what Rating types can be
 	// indexed by (indexing looks like: ex. myRating[braille])
